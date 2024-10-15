b575d28ab1793898ca246bc5cbcbdba225589bb0
Demetrius D. <demetriusthornton@gmail.com>
2024-10-14 20:58:20 -0400

    adding cart project changes.

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -57,23 +57,129 @@
 
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
 
-
 /* The following is for running unit tests. 
    To fully complete this project, it is expected that all tests pass.
    Run the following command in terminal to run tests
    npm run test
 */
 
-module.exports = {
-   products,
-   cart,
-   addProductToCart,
-   increaseQuantity,
-   decreaseQuantity,
-   removeProductFromCart,
-   cartTotal,
-   pay, 
-   emptyCart,
-   /* Uncomment the following line if completing the currency converter bonus */
-   // currency
+// module.exports = {
+//   products,
+//   cart,
+//   addProductToCart,
+//   increaseQuantity,
+//   decreaseQuantity,
+//   removeProductFromCart,
+//   cartTotal,
+//   pay,
+//   emptyCart,
+//   /* Uncomment the following line if completing the currency converter bonus */
+//   // currency
+// };
+
+// Product array
+const products = [
+  {
+    name: "Cherry",
+    price: 5.99,
+    quantity: 0,
+    productId: 1001,
+    image: "images/cherry.jpg",
+  },
+  {
+    name: "Orange",
+    price: 3.99,
+    quantity: 0,
+    productId: 1002,
+    image: "images/orange.jpg",
+  },
+  {
+    name: "Strawberry",
+    price: 4.99,
+    quantity: 0,
+    productId: 1003,
+    image: "images/strawberry.jpg",
+  },
+];
+
+// Cart array
+let cart = [];
+
+// Function to add product to cart
+function addProductToCart(productId) {
+  const product = products.find((p) => p.productId === productId);
+  if (product) {
+    const cartItem = cart.find((item) => item.productId === productId);
+    if (cartItem) {
+      cartItem.quantity++;
+    } else {
+      cart.push({ ...product, quantity: 1 });
+    }
+    product.quantity++;
+  }
 }
+
+// Function to increase quantity
+function increaseQuantity(productId) {
+  const cartItem = cart.find((item) => item.productId === productId);
+  if (cartItem) {
+    cartItem.quantity++;
+    const product = products.find((p) => p.productId === productId);
+    product.quantity++;
+  }
+}
+
+// Function to decrease quantity
+function decreaseQuantity(productId) {
+  const cartItem = cart.find((item) => item.productId === productId);
+  if (cartItem) {
+    cartItem.quantity--;
+    const product = products.find((p) => p.productId === productId);
+    product.quantity--;
+    if (cartItem.quantity === 0) {
+      removeProductFromCart(productId);
+    }
+  }
+}
+
+// Function to remove product from cart
+function removeProductFromCart(productId) {
+  const index = cart.findIndex((item) => item.productId === productId);
+  if (index !== -1) {
+    const product = products.find((p) => p.productId === productId);
+    product.quantity = 0;
+    cart.splice(index, 1);
+  }
+}
+
+// Function to calculate cart total
+function cartTotal() {
+  return cart.reduce((total, item) => total + item.price * item.quantity, 0);
+}
+
+// Function to empty cart
+function emptyCart() {
+  cart.forEach((item) => {
+    const product = products.find((p) => p.productId === item.productId);
+    product.quantity = 0;
+  });
+  cart = [];
+}
+
+// Function to handle payment
+function pay(amount) {
+  const total = cartTotal();
+  return amount - total;
+}
+
+module.exports = {
+  products,
+  cart,
+  addProductToCart,
+  increaseQuantity,
+  decreaseQuantity,
+  removeProductFromCart,
+  cartTotal,
+  pay,
+  emptyCart,
+};
